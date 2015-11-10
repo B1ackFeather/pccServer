89bd5a4744654a39862b7fc3171132c445adc039
JY Qiu <jyqiu1216@gmail.com>
2015-11-11 00:10:15 +0800

    first commit

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    var db = req.db;
+    db.collection('parent').find({}).toArray(function (err, items) {
+        res.json(items);
+    });
+});
+
+router.get('/login', function(req, res, next) {
+    var db = req.db;
+    db.collection('parent').findOne({'parentID':req.query.ID, 'password':req.query.password}, function (err, items) {
+        if (items == null){
+        	res.json({'sta':false});
+        }
+        else{
+        	items.sta = true;
+        	res.json(items);
+        }
+    });
+});
+
+module.exports = router;
