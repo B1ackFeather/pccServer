29f4bd41d5402ff3211f0ba13db08f4b8f24a76c
JY Qiu <jyqiu1216@gmail.com>
2015-11-11 23:56:46 +0800

    create assignment.js

---
+++ routes/assment.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    var db = req.db;
+    db.collection('assignment').find({}).toArray(function (err, items) {
+        res.json(items);
+    });
+});
+
+router.get('/get', function(req, res, next) {
+    var db = req.db;
+    if(req.query.classnum)
+        db.collection('assignment').find({'class':req.query.classnum}).toArray(function (err, items){
+           res.json(items);
+        }); 	
+    else if(req.query.teacher)
+        db.collection('assignment').find({'creator':req.query.teacher}).toArray(function (err, items){
+           res.json(items);
+        }); 
+});
+
+
+
+module.exports = router;
