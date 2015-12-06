a7837dc0a5359befeabb06674580450754dcdf4e
JY Qiu <jyqiu1216@gmail.com>
2015-12-06 16:14:35 +0800

    add get schedule api

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET schedule listing. */
 router.get('/', function(req, res, next) {
     var db = req.db;
     db.collection('schedule').find({}).toArray(function (err, items) {
         res.json(items);
     });
 });
+
+/* GET shedule by id listing. */
+router.get('/get', function(req, res, next) {
+    var db = req.db;
+    if(req.query.classid)
+	    db.collection('schedule').find({'classid':req.query.classid},{'_id':0}).toArray(function (err, items) {
+	   		res.json(items);
+	    });    
+    else if(req.query.teacherid)
+    	db.collection('schedule').find({'teacherid':req.query.teacherid},{'_id':0}).toArray(function (err, items) {
+	   		res.json(items);
+	    }); 
+   	else{
+		var err = new Error('Not Found');
+  		err.status = 404;
+  		next(err);
+ 	}
+		
+  });
 
 // router.get('/get', function(req, res, next) {
 //     var db = req.db;
